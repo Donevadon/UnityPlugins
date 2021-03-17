56e990a7a60a49e97537908a71e70b7cdda28c5a
Roman <donevadon@gmail.com>
2021-03-17 14:31:41 +0300

    Plugin

--- UnityPlugin/ContactList/src/main/java/com/oland/contactlist/IContactContainer.java
+++
@@ -1,9 +0,0 @@
-package com.oland.contactlist;
-
-public interface IContactContainer
-{
-    int createNewContact(String name);
-    void setNumber(int index,String number);
-    void setEmail(int index,String email);
-    void setEvent(int index,String _event);
-}
--- UnityPlugin/Log/src/main/java/com/oland/log/Log.java
+++
@@ -1,8 +0,0 @@
-package com.oland.log;
-
-public class Log {
-    public static void debugLog(String tag,String message)
-    {
-        android.util.Log.i(tag,message);
-    }
-}
